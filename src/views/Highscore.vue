359b9aa6af4d7dc2cc6a4771c7b1cb5506567040
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-15 19:10:59 +0100

    Implement highscore list of users.

--- src/views/Highscore.vue
+++ src/views/Highscore.vue
@@ -1,14 +1,62 @@
 <template>
 <h1> Highscore List</h1>
+  <div class = "highscore-list">
+    <table>
+      <thead>
+        <tr>
+          <th>Player</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+      <tbody>
+        <div v-for="user in users" :key="user.id">
+          <tr>
+            <td>{{ user.name }}</td>
+            <td>{{ user.highscore }}</td>
+          </tr>
+        </div>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Highscore'
+  name: 'Highscore',
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    loadPlayers () {
+      const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
+      const endpoint = baseUrl + '/api/v1/users'
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => result.forEach(user => {
+          this.users.push(user)
+        }))
+        .catch(error => console.log('error', error))
+    }
+
+  },
+  mounted () {
+    this.loadPlayers()
+  }
 
 }
 </script>
 
 <style scoped>
+  table{
+    margin-left: auto;
+    margin-right: auto;
+  }
 
 </style>
