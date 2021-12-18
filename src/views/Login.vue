60da51d48db83aece73ccc26d654ffb558254197
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-18 21:54:10 +0100

    Create User.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,40 @@
 <template>
 <h1>Login / create account</h1>
+  <input v-model="username" type="text">
+  <button type="button" @click = "save()">Create</button>
 </template>
 
 <script>
 export default {
-  name: 'Login'
-
+  name: 'Login',
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    save () {
+      const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
+      const endpoint = baseUrl + '/api/v1/users'
+      const data = {
+        name: this.username,
+        highscore: 0
+      }
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+      alert(this.username + ' created')
+      this.username = ''
+    }
+  }
 }
 </script>
 
