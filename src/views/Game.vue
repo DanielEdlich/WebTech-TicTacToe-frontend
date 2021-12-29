c49eb404ec28c234a9a2c37a92d7ffbe65c7e618
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-29 23:10:59 +0100

    Implement single player capability.

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -64,22 +64,12 @@ export default {
         .catch(error => console.log('error', error))
     },
 
-    gridToBoard () {
-      for (let c = 0; c < this.game.grid.length; c++) {
-        this.board[Math.floor(c / 3)][c % 3] = this.game.grid.charAt(c)
-      }
-    },
-
-    boardToGrid () {
-
-    },
-
     createGame () {
       const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
-      const endpoint = baseUrl + '/api/v1/users'
+      const endpoint = baseUrl + '/api/v1/games'
       const data = {
-        player1_id: 1,
-        player2_id: 2,
+        player1_id: this.requestUId1,
+        player2_id: this.requestUId2,
         isFinished: false,
         grid: '---------'
       }
@@ -95,8 +85,109 @@ export default {
         .then(result => console.log(result))
         .catch(error => console.log('error', error))
     },
+
+    updateGame () {
+      const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
+      const endpoint = baseUrl + '/api/v1/games/' + this.game.id
+      const data = {
+        player1_id: this.game.player_1_id(),
+        player2_id: this.game.player_2_id(),
+        isFinished: this.game.isFinished(),
+        grid: this.boardToGrid()
+      }
+      const requestOptions = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+    },
+
+    gridToBoard () {
+      for (let c = 0; c < this.game.grid.length; c++) {
+        this.board[Math.floor(c / 3)][c % 3] = this.game.grid.charAt(c)
+      }
+    },
+
+    boardToGrid () {
+      let str
+      for (let m = 0; m < this.board.length; m++) {
+        for (let n = 0; n < this.board[m].length; n++) {
+          str += this.board[m][n]
+        }
+      }
+
+      return str
+    },
+
     processAction (m, n) {
-      this.board[m][n] = 'x'
+      /* if (player === this.game.player_1_id) {
+        this.board[m][n] = 'x'
+      } else {
+        this.board[m][n] = 'o' */
+      if (this.board[m][n] === '-' && !this.game.isFinished) {
+        this.board[m][n] = 'X'
+        // console.log(this.hasWinner())
+        // console.log(this.isFull())
+        if (!this.hasWinner() || !this.isFull()) {
+          this.opponentMove()
+        }
+      }
+    },
+    opponentMove () {
+      let x
+      let y
+      do {
+        x = Math.floor(Math.random() * 3)
+        y = Math.floor(Math.random() * 3)
+        // console.log(x + ' ' + y)
+      } while ((this.board[x][y] === 'X') || (this.board[x][y] === 'O'))
+
+      this.board[x][y] = 'O'
+    },
+
+    hasWinner () {
+      // horizontally
+      for (let x = 0; x < this.board.length; x++) {
+        if (this.board[x][0] === this.board[x][1] && this.board[x][1] === this.board[x][2] && this.board[x][0] !== '-') {
+          this.game.isFinished = true
+          return true
+        }
+      }
+
+      // vertically
+      for (let y = 0; y < this.board.length; y++) {
+        if (this.board[0][y] === this.board[1][y] && this.board[1][y] === this.board[2][y] && this.board[0][y] !== '-') {
+          this.game.isFinished = true
+          return true
+        }
+      }
+
+      // diagonally
+      if ((this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2] && this.board[0][0] !== '-') ||
+        (this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0] && this.board[0][2] !== '-')) {
+        this.game.isFinished = true
+        return true
+      }
+
+      return false
+    },
+
+    isFull () {
+      for (let x = 0; x < this.board.length; x++) {
+        for (let y = 0; y < this.board[x].length; y++) {
+          if (this.board[x][y] === '-') {
+            return false
+          }
+        }
+        this.game.isFinished = true
+        return true
+      }
     }
   }
 }
@@ -111,7 +202,9 @@ export default {
   .cell{
     width: 64px;
     height: 64px;
-    border: 2px solid rgb(75,75,75);
+    border-width: 1px;
+    border-style: solid;
+    border-color:  black;
   }
 
 </style>
