8c0cdc1c9e717420dea2dab77d27e97d94925989
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-30 20:13:41 +0100

    Refactor Highscore.vue.

---
+++ src/components/HighscoreList.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="table align-middle">
+    <thead>
+    <tr>
+      <th scope="col">Player</th>
+      <th scope="col">Points</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-if="users.length === 0">
+      <td colspan="2">No highscores yet</td>
+    </tr>
+    <tr v-for="user in users" :key="user.id">
+      <td> {{ user.name }}</td>
+      <td> {{ user.highscore}}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'HighscoreList',
+  props: {
+    users: []
+  }
+}
+</script>
+<style scoped>
+table {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
--- src/views/Highscore.vue
+++ src/views/Highscore.vue
@@ -1,29 +1,15 @@
 <template>
 <h1> Highscore List</h1>
-  <div class = "highscore-list">
-    <table class="table align-middle">
-      <thead>
-        <tr>
-          <th scope="col">Player</th>
-          <th scope="col">Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-if="users.length === 0">
-          <td colspan="2">No highscores yet</td>
-        </tr>
-        <tr v-for="user in users" :key="user.id">
-          <td > {{ user.name }} </td>
-          <td > {{ user.highscore}} </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+
+    <HighscoreList :users="users"/>
+
 </template>
 
 <script>
+import HighscoreList from '@/components/HighscoreList'
 export default {
   name: 'Highscore',
+  components: { HighscoreList },
   data () {
     return {
       users: []
