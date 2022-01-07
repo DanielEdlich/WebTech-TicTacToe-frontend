d6f74cac643e4b74b684352d8603400aa64b6a74
tobia <tobias_armbruster@hotmail.de>
2022-01-07 09:32:41 +0100

    Register Vue frontend valid.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,22 @@
 <template>
-  <h1>Login</h1>
-  <form class="<<!--position-absolute start-50 justify-content-center--> w-50 mx-auto">
+  <h1>Register</h1>
+  <form id="users-create-form" class="<<!--position-absolute start-50 justify-content-center--> w-50 mx-auto needs-validation" novalidate>
     <div class="mb-3">
       <label for="InputUsername" class="form-label">Username</label>
-      <input v-model="username" type="text" class="form-control" id="InputUsername">
+      <input v-model="username" type="text" class="form-control" id="InputUsername" required>
+      <div class="invalid-feedback">
+        Please provide the username.
+      </div>
     </div>
     <div class="mb-3">
       <label for="InputPassword" class="form-label">Password</label>
-      <input v-model="password" type="password" class="form-control" id="InputPassword">
+      <input v-model="password" type="password" class="form-control" id="InputPassword" required>
+      <div class="invalid-feedback">
+        Please provide the password.
+      </div>
+    </div>
+    <div v-if="error" class="alert alert-danger" role="alert">
+      {{error}}
     </div>
     <p>
     </p>
@@ -27,12 +36,13 @@ export default {
   data () {
     return {
       username: '',
-      password: ''
+      password: '',
+      error: ''
     }
   },
   methods: {
     async save () {
-      if (this.username !== '' && this.password !== '') {
+      if (this.validate()) {
         const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
         const endpoint = baseUrl + '/api/v1/signup'
         const data = {
@@ -48,6 +58,35 @@ export default {
           body: JSON.stringify(data)
         }
         await fetch(endpoint, requestOptions)
+
+          .then(async response => {
+            console.log(response)
+            if (response.ok) {
+              alert(this.username + ' created')
+              // alert(this.username + ' logged in')
+              await router.push('/login')
+            } else if (response.status === 400) {
+              response = await response.json()
+              if (response.errors) {
+                response.errors.forEach(error => {
+                  this.error += error.defaultMessage + '\n'
+                })
+              } else {
+                this.error = response.message
+              }
+            } else {
+              this.error = 'Unknown error occurred'
+            }
+          })
+          .catch(error => {
+            console.log('error', error)
+            this.error = error.defaultMessage
+          })
+      } else {
+        console.log('username and password can not be empty')
+      }
+    },
+    /* await fetch(endpoint, requestOptions)
           .then(response => response.json())
           .then(result => console.log(result))
           .catch(error => console.log('error', error))
@@ -57,6 +96,11 @@ export default {
       } else {
         console.log('username and password can not be empty')
       }
+    }, */
+    validate () {
+      const form = document.getElementById('users-create-form')
+      form.classList.add('was-validated')
+      return form.checkValidity()
     }
   }
 }
